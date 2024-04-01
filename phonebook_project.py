f423e7e68255404cc90f9bbf230efe7c6d236853
Soniq <rodion.karokhin@gmail.com>
2024-04-01 15:12:47 +0300

    new version

--- phonebook_project.py
+++ phonebook_project.py
@@ -23,49 +23,84 @@ def save():
     with open('phonebook.txt', 'w') as file:
         for name, data in directory.items():
             file.write(f"{name},{data["phone"]},{data["birthday"]},{data["email"]}\n")
+        print("Изменения успешно сохранены!")
 
-def add(name, phone, birthday, email):
-    directory[name]={}
-    try:
-        directory[name]["phone"].append(phone)
-    except:
-        directory[name]["phone"] = []
-        directory[name]["phone"].append(phone)
-    try:
-        directory[name]["birthday"].append(birthday)
-    except:
-        directory[name]["birthday"] = []
-        directory[name]["birthday"].append(birthday)
-    try:
-        directory[name]["email"].append(email)
-    except:
-        directory[name]["email"] = []
-        directory[name]["email"].append(email)
-
-def delete(name):
+def add():
+    choice = input("Что вы хотите добавить? (контакт - contact , значение - value ) : ")
+    if choice == "contact":
+        name = input("Введите имя контакта: ")
+        phone = input("Введите телефон: ")
+        birthday = input("Введите дату рождения: ")
+        email = input("Введите электронную почту: ")
+        directory[name]={}
+        try:
+            directory[name]["phone"].append(phone)
+        except:
+            directory[name]["phone"] = []
+            directory[name]["phone"].append(phone)
+        try:
+            directory[name]["birthday"].append(birthday)
+        except:
+            directory[name]["birthday"] = []
+            directory[name]["birthday"].append(birthday)
+        try:
+            directory[name]["email"].append(email)
+        except:
+            directory[name]["email"] = []
+            directory[name]["email"].append(email)
+        print("Данные успешно добавлены в справочник!")
+    elif choice == "value":
+        name = input("Введите имя контакта: ")
+        option = input("Введите параметр, в который хотите добавить данные (phone, birthday, email): ")
+        value = input("Введите значение параметра: ")
+        try:
+            directory[name][option].append(value)
+            print("Данные успешно добавлены в справочник!")
+        except:
+            print("Что-то пошло не так... Проверьте введенные данные и повторите попытку")
+    else:
+        print("Что-то пошло не так... Проверьте введенные данные и повторите попытку")
+def delete():
+    name = input("Введите имя для удаления: ")
     choice = input("Что вы хотите удалить? (контакт - contact , значение - value ) : ")
     if choice == "contact":
         if name in directory:
             del directory[name]
+            print("Контакт успешно удален!")
         else:
             print("Контакт не найден.")
     elif choice == "value":
         option = input("Введите параметр, из которого хотите удалить данные (phone, birthday, email): ")
         value = input("Введите данные для удаления: ")
-        temp = directory[name][option].index(value)
-        del directory[name][option][temp]
-
-def change(name, option, value_old, value_new):
+        try:
+            temp = directory[name][option].index(value)
+            del directory[name][option][temp]
+            print("Данные успешно удалены!")
+        except:
+            print("Что-то пошло не так... Проверьте введенные данные и повторите попытку")
+    else:
+        print("Что-то пошло не так... Проверьте введенные данные и повторите попытку")
+def change():
+    name = input("Введите имя для изменения: ")
+    option = input("Введите параметр, который хотите изменить (name, phone, birthday, email): ")
+    value_old = input("Введите значение для изменения: ")
+    value_new = input("Введите новое значение: ")
     if name in directory:
         if option=="name":
             directory[value_new] = directory.pop(name)
+            print("Данные успешно изменены!")
         else:
-            temp = directory[name][option].index(value_old)
-            directory[name][option][temp] = value_new
+            try:
+                temp = directory[name][option].index(value_old)
+                directory[name][option][temp] = value_new
+                print("Данные успешно изменены!")
+            except:
+                print("Что-то пошло не так... Проверьте введенные параметры и повторите попытку")
     else:
         print("Контакт не найден.")
 
 def show():
+    print("Ваш телефонный справочник: ")
     for name, data in directory.items():
         print(f"{name}: {data}")
 
@@ -78,48 +113,38 @@ def search():
             if key==value1:
                 print(f"{key}: {directory[key]}")   
     else:
-        print("Результаты по вашему запросу: ")
-        for key, value in directory.items():
-            for i in value[option]:
-                if i == value1:
-                    print(f"{key}: {directory[key]}")    
+        try:
+            print("Результаты по вашему запросу: ")
+            for key, value in directory.items():
+                for i in value[option]:
+                    if i == value1:
+                        print(f"{key}: {directory[key]}")    
+        except:
+            print("Что-то пошло не так... Проверьте введенные параметры и повторите попытку")
+            
 directory=load()
 
 choice = input("Введите /start, чтобы начать: ")
 if choice == "/start":
     while True:
-        command = input("Ваш запрос: ")
+        command = input("\nВаш запрос: ")
         
         if command == '/add':
-            name = input("Введите имя: ")
-            phone = input("Введите телефон: ")
-            birthday = input("Введите дату рождения: ")
-            email = input("Введите электронную почту: ")
-            add(name, phone, birthday, email)
-            print("Контакт успешно добавлен!")
+            add()
             
         elif command == '/delete':
-            name = input("Введите имя для удаления: ")
-            delete(name)
-            print("Данные успешно удалены!")
+            delete()
             
         elif command == '/change':
-            name = input("Введите имя для изменения: ")
-            option = input("Введите параметр, который хотите изменить (name, phone, birthday, email): ")
-            value_old = input("Введите значение для изменения: ")
-            value_new = input("Введите новое значение: ")
-            change(name, option, value_old, value_new)
-            print("Контакт успешно изменен!")
+            change()
             
         elif command == '/show':
-            print("Ваш телефонный справочник: ")
             show()
         
         elif command == '/search':
             search()
         elif command == '/save':
             save()
-            print("Изменения успешно сохранены!")
             
         elif command == '/quit':
             print("Вы точно хотите выйти? Убедитесь, что сохранили нужные вам изменения. (введите 'yes', если хотите выйти)")
